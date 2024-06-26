8f1c24f17107348dc75e57ddb013dcb02e8803ae
koleajeolayinka <koleajeolayinka@gmail.com>
2024-06-26 16:18:21 +0100

    Add global styles and base layout configuration
    Introduce a new `globals.css` for tailwind and color scheme variables. Also, an overall layout configuration (`layout.tsx`) is set up with standard metadata and loading of Google's Inter font. Setup `Home` page (`page.tsx`) with basic components layout is also done.

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({subsets: ["latin"]});
+
+export const metadata: Metadata = {
+    title: "Enum",
+    description: "",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <link rel="icon" href="/logo.svg" type="image/svg+xml"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
