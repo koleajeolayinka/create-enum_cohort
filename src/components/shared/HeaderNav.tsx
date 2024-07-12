29d758eed7793fd58bab1c150ea203b9b53401d7
koleajeolayinka <koleajeolayinka@gmail.com>
2024-07-12 14:09:26 +0100

    refactor(HeaderNav): Update active link highlighting logic

--- src/components/shared/HeaderNav.tsx
+++ src/components/shared/HeaderNav.tsx
@@ -1,25 +1,31 @@
 'use client';
-import { useState } from 'react';
-import Link from 'next/link';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const HeaderNav = () => {
-    const navItems: string[] = ['Home', 'Workspace', 'Resources Library'];
-    const [activeNav, setActiveNav] = useState('');
+    const navItems = ["Home", "Workspace", "Resources Library"];
+    const pathname = usePathname(); // Get the current pathname
+
+    const isActive = (item: string) => {
+        // Check if pathname starts with the item's path or matches exactly
+        const itemPath = `/${item.toLowerCase().replace(/ /g, "-")}`;
+        return pathname === itemPath || pathname.startsWith(`${itemPath}/`);
+    };
+
     return (
         <nav className="text-enumGrey">
-            <ul className="flex h-13 gap-3 text-base font-semibold leading-[175%] max-lg:space-x-1 space-x-3  max-lg:gap-[1px]  ">
-                {navItems.map((item: string) => (
-                    <li key={item} className={item === activeNav ? 'border-b-4 border-enumBlue rounded-[4px]' : ''}>
-                        <Link href={`/${item.toLowerCase().replace(/ /g, '-')}`}>
+            <ul className="flex h-13 gap-3 text-base font-semibold leading-[175%] max-lg:space-x-1 space-x-3  max-lg:gap-[1px]">
+                {navItems.map((item) => (
+                    <li key={item} className={isActive(item) ? "border-b-4 border-enumBlue rounded-[4px]" : ""}>
+                        <Link href={`/${item.toLowerCase().replace(/ /g, "-")}`}>
                             <button
                                 className={
-                                    item === activeNav
-                                        ? 'p-2 px-6 text-enumBlue bg-[#F6FCFF] rounded-[300px] m-2   max-md:p1 '
-                                        : 'p-2 px-6 m-2 '
+                                    isActive(item)
+                                        ? "p-2 px-6 text-enumBlue bg-[#F6FCFF] rounded-[300px] m-2   max-md:p1 "
+                                        : "p-2 px-6 m-2 "
                                 }
-                                onClick={() => setActiveNav(item)}
                             >
-                                <h4 className={'max-[1024px]:text-xxs'}>{item}</h4>
+                                <h4 className={"max-[1024px]:text-xxs"}>{item}</h4>
                             </button>
                         </Link>
                     </li>
