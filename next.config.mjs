d0e6eabcafe99137d026ed38becc57222a57138a
koleajeolayinka <koleajeolayinka@gmail.com>
2024-07-29 14:47:30 +0100

    Update configuration files and dependencies
                   - Modified
    ext.config.mjs
                   - Modified package-lock.json
                   - Modified package.json
                   - Modified 	ailwind.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true, // Enable app directory for Next.js 13
-        serverComponentsExternalPackages: ['@prisma/client'], // Allow server components to import from `@prisma/client`
-        fontLoaders: [
-            { loader: '@next/font/google', options: { subsets: ['latin'] } },
-        ],
-    },
+    // experimental: {
+    //     appDir: true, // Enable app directory for Next.js 13
+    //     serverComponentsExternalPackages: ['@prisma/client'], // Allow server components to import from `@prisma/client`
+    //     fontLoaders: [
+    //         { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    //     ],
+    // },
 };
 
 export default nextConfig;
